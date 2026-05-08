9ad8cc478e14385673ddda186a2ee2257fe8cb87
AaryanH-star <aaryanh.cse25@sbjit.edu.in>
2026-05-08 15:08:28 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// HAMBURGER MENU
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+// SMOOTH SCROLL
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href"))
+        .scrollIntoView({
+            behavior:"smooth"
+        });
+
+    });
+
+});
